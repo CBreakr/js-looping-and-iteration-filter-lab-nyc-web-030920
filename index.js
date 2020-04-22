3dff45eb8421ed5a9b398d3133b8b7fe2f7d9f4e
CBreakr <github email address>
2020-04-22 13:17:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file
+
+function findMatching(drivers, name){
+    return drivers.filter(driver => {
+        return name.toLowerCase() === driver.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, first){
+    return drivers.filter(driver => {
+        return driver.toLowerCase().startsWith(first.toLowerCase());
+    });
+}
+
+function matchName(drivers, name){
+    return drivers.filter(driver => {
+        return driver.name === name;
+    });
+}
